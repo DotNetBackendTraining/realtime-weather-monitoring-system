84f648607a267ec618ba6632454e1d5d3d8931dc
Izzat Alsharif <work@izzatalsharif.com>
2024-03-16 20:08:49 +0200

    Add documentation

--- RealTimeWeatherMonitoringApp/Application/Interfaces/Service/IBotNotificationService.cs
+++ RealTimeWeatherMonitoringApp/Application/Interfaces/Service/IBotNotificationService.cs
@@ -2,6 +2,9 @@ using RealTimeWeatherMonitoringApp.Domain.Common;
 
 namespace RealTimeWeatherMonitoringApp.Application.Interfaces.Service;
 
+/// <summary>
+/// Allows subscription to bot notifications
+/// </summary>
 public interface IBotNotificationService
 {
     event EventHandler<BotEventArgs> OnBotNotification;
