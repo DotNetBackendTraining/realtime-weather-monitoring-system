71124a5c7ef134a0eaa033241a91206352d4915c
Izzat Alsharif <work@izzatalsharif.com>
2024-03-16 16:09:42 +0200

    Introduce & Implement BotController

--- RealTimeWeatherMonitoringApp/Domain/Common/BotEventArgs.cs
+++ RealTimeWeatherMonitoringApp/Domain/Common/BotEventArgs.cs
@@ -2,6 +2,6 @@ namespace RealTimeWeatherMonitoringApp.Domain.Common;
 
 public class BotEventArgs : EventArgs
 {
-    public string Message { get; set; }
+    public string Message { get; }
     public BotEventArgs(string message) => Message = message;
 }
