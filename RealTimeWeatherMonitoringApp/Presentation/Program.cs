f3986625a8efcba4e1c79d403acee8d7a4ee3fca
Izzat Alsharif <work@izzatalsharif.com>
2024-03-16 20:12:23 +0200

    Implement program entry point with proper initialization

--- RealTimeWeatherMonitoringApp/Presentation/Program.cs
+++ RealTimeWeatherMonitoringApp/Presentation/Program.cs
@@ -1,4 +1,18 @@
+using Microsoft.Extensions.DependencyInjection;
+using RealTimeWeatherMonitoringApp.Application.Interfaces;
+using RealTimeWeatherMonitoringApp.Application.Interfaces.Service;
+using RealTimeWeatherMonitoringApp.Domain.Interfaces.Service;
+using RealTimeWeatherMonitoringApp.Domain.Models;
 using RealTimeWeatherMonitoringApp.Presentation;
 
+// Inject Dependencies
 var provider = DependencyInjector.CreateServiceProvider();
+
+// Initialize Bot Events
+var weatherBotPublisher = provider.GetRequiredService<IBotPublishingService>();
+var weatherDataNotifier = provider.GetRequiredService<IDataChangeNotifier<WeatherData>>();
+var weatherEventManager = provider.GetRequiredService<IBotEventManager<WeatherData>>();
+weatherEventManager.Attach(weatherDataNotifier, weatherBotPublisher);
+
+// Start
 new UserController(provider).Start();
