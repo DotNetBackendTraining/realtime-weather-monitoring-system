5488b87ac6025f74163ad5a1a70547cd14f90af8
Izzat Alsharif <work@izzatalsharif.com>
2024-03-18 15:11:14 +0200

    Initialize json options lazily

--- RealTimeWeatherMonitoringApp/Infrastructure/Factory/ConfigurationFactory.cs
+++ RealTimeWeatherMonitoringApp/Infrastructure/Factory/ConfigurationFactory.cs
@@ -7,24 +7,24 @@ namespace RealTimeWeatherMonitoringApp.Infrastructure.Factory;
 
 public class ConfigurationFactory : IConfigurationFactory
 {
-    private const string ConfigurationFilePath = "Infrastructure/Configuration/configuration.json";
+    private const string BotConfigurationFilePath = "Infrastructure/Configuration/configuration.json";
+
+    private static readonly Lazy<JsonSerializerOptions> JsonOptions = new(() => new JsonSerializerOptions
+    {
+        PropertyNameCaseInsensitive = true,
+        Converters = { new JsonStringEnumConverter(JsonNamingPolicy.CamelCase) }
+    });
 
     private readonly Lazy<IEnumerable<BotConfiguration>> _botConfigurations = new(() =>
     {
         var baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
-        var fullPath = Path.Combine(baseDirectory, ConfigurationFilePath);
+        var fullPath = Path.Combine(baseDirectory, BotConfigurationFilePath);
         var jsonText = File.ReadAllText(fullPath);
 
-        var options = new JsonSerializerOptions
-        {
-            PropertyNameCaseInsensitive = true,
-            Converters = { new JsonStringEnumConverter(JsonNamingPolicy.CamelCase) }
-        };
-        var botDict = JsonSerializer.Deserialize<Dictionary<string, BotConfiguration>>(jsonText, options);
-        if (botDict == null) return Enumerable.Empty<BotConfiguration>();
-
-        return botDict
-            .Select(kvp => kvp.Value with { Name = kvp.Key });
+        var botDict = JsonSerializer.Deserialize<Dictionary<string, BotConfiguration>>(jsonText, JsonOptions.Value);
+        return botDict == null
+            ? Enumerable.Empty<BotConfiguration>()
+            : botDict.Select(kvp => kvp.Value with { Name = kvp.Key });
     });
 
     public IEnumerable<BotConfiguration> CreateBotConfigurations() => _botConfigurations.Value;
