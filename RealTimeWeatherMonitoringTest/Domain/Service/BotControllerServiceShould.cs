a182bbb67f6bda8d421ff304de6a97076a3b6f51
Izzat Alsharif <work@izzatalsharif.com>
2024-03-22 22:19:33 +0200

    Add test and customizers

--- RealTimeWeatherMonitoringTest/Domain/Service/BotControllerServiceShould.cs
+++ RealTimeWeatherMonitoringTest/Domain/Service/BotControllerServiceShould.cs
@@ -1,12 +1,15 @@
+using AutoFixture.AutoMoq;
 using AutoFixture.Xunit2;
 using FluentAssertions;
 using Moq;
+using RealTimeWeatherMonitoringApp.Domain.Common;
 using RealTimeWeatherMonitoringApp.Domain.Interfaces.Repository;
 using RealTimeWeatherMonitoringApp.Domain.Interfaces.Service;
 using RealTimeWeatherMonitoringApp.Domain.Models;
 using RealTimeWeatherMonitoringApp.Domain.Models.Controller;
 using RealTimeWeatherMonitoringApp.Domain.Service;
 using RealTimeWeatherMonitoringTest.Common;
+using RealTimeWeatherMonitoringTest.Domain.Common;
 using RealTimeWeatherMonitoringTest.Domain.Models;
 using Xunit;
 
@@ -47,5 +50,25 @@ public class BotControllerServiceShould
         botControllers.Should().AllBeOfType<PublisherBotController<TestData>>()
             .And.HaveCount(bots.Count)
             .And.OnlyHaveUniqueItems();
+    }
+
+    [Theory, CustomAutoData(
+         typeof(AutoMoqCustomization),
+         typeof(ReactiveBotControllerCustomization<TestData>))]
+    public void GetBotControllers_WithPublishingService_ControllersEventsPublishedOnService(
+        List<Bot<TestData>> bots,
+        Mock<IBotPublishingService> publishingServiceMock,
+        DataChangeEventArgs<TestData> dataChangeEventArgs,
+        [Frozen] Mock<IBotRepository<TestData>> repositoryMock,
+        BotControllerService<TestData> service)
+    {
+        repositoryMock.Setup(r => r.GetAll()).Returns(bots);
+        var botControllers = service.GetBotControllers(publishingServiceMock.Object);
+
+        foreach (var controller in botControllers)
+        {
+            var eventArgs = controller.React(dataChangeEventArgs);
+            publishingServiceMock.Verify(s => s.Publish(eventArgs), Times.Once);
+        }
     }
 }
